3d06cee0f238f912f3cf0c6d05b1bf85d17be615
Eduard Spousta <eduard.spousta@icloud.com>
2023-07-26 14:16:27 +0200

    Resource folder + paths, add new "Custom" Font, Price layout fixed

--- src/file/ImageEditor.java
+++ src/file/ImageEditor.java
@@ -3,12 +3,11 @@ package file;
 import model.Data;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLOutput;
+import java.io.InputStream;
 
 public class ImageEditor {
     private BufferedImage imageIn;
@@ -20,7 +19,7 @@ public class ImageEditor {
             String temp;
 
             //Load image
-            imageIn = ImageIO.read(new File("giftCard.png"));
+            imageIn = ImageIO.read(new File("./src/resources/img/giftCard.png"));
             System.out.println("image load success");
 
             //New image setup
@@ -29,19 +28,25 @@ public class ImageEditor {
             System.out.println(imageIn.getHeight());
             g.drawImage(imageIn, 0,0, null);
 
+            //Load Specific font
+            //https://fonts.google.com/noto/specimen/Noto+Sans+Grantha?preview.text=2000&preview.text_type=custom&query=grant&category=Sans+Serif
+            InputStream inputStream = ImageEditor.class.getResourceAsStream("/resources/font/NotoSansGrantha-Regular.ttf");
+            Font myFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+            myFont = myFont.deriveFont(265f);
+
             //draw price - depends on int value (number of digits)
-            g.setFont(new Font("Tw Cen MT", Font.PLAIN, 288));
+            g.setFont(myFont);
             g.setColor(new Color(48,48,48));
             if(data.getPrice()<1000){
-                g.drawString(data.getPrice() + " Kč", 910,  640);
+                g.drawString(data.getPrice() + " Kč", 920,  640);
                 System.out.println("100-999");
             } else if (data.getPrice()<10000){
-                g.drawString(data.getPrice() + " Kč", 810,  640);
+                g.drawString(data.getPrice() + " Kč", 820,  640);
                 System.out.println("1000-9999");
             } else if(data.getPrice()<100000){
                 temp = String.valueOf(data.getPrice());
                 temp = temp.substring(0,2) + " " + temp.substring(2);
-                g.drawString(temp+ " Kč", 710,  640);
+                g.drawString(temp+ " Kč", 720,  640);
                 System.out.println("10000-99999");
             }
 
@@ -70,6 +75,10 @@ public class ImageEditor {
 
         } catch (IOException e) {
             System.out.println("Can not load image giftCard.png. Please contact admin" + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (FontFormatException e) {
+            System.out.println("Can not load font NotoSansGrantha-Regular.ttf. Please contact admin" + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
